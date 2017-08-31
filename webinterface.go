83940006f9ee6bda8ccc201fe779b85883673bb1
Vladimir <vladimir@pravdabox.net>
2017-08-31 14:03:28 +0200

    baremetal

--- webinterface.go
+++ webinterface.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"net/http"
+	"os"
+)
+
+// VERSION holds the version
+const VERSION = "0.1.0"
+
+var (
+	listenAddress = flag.String("l", "127.0.0.1:8080", "Web interface listen address")
+	version       = flag.Bool("v", false, "Display version")
 )
 
 func main() {
-	fmt.Println("Hello.")
+	flag.Parse()
+
+	if *version {
+		fmt.Println(VERSION)
+		os.Exit(0)
+	}
+
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.ListenAndServe(*listenAddress, nil)
 }
