1f5dbf6652e35d660cd153b35702fdd03059b546
Vladimir <vladimir@pravdabox.net>
2017-09-02 15:11:21 +0200

    only one webserver. jeezz, why not earlier?

--- webinterface.go
+++ webinterface.go
@@ -11,15 +11,14 @@ import (
 )
 
 // VERSION holds the version
-const VERSION = "0.6.0"
+const VERSION = "0.7.0"
 
 // MAXFORKS limits the forks of websockets
 const MAXFORKS = 10
 
 var (
-	listenAddress   = flag.String("l", "127.0.0.1:8080", "Web interface listen address")
-	wsListenAddress = flag.String("wsl", "127.0.0.1:8088", "Web sockets listen address")
-	version         = flag.Bool("v", false, "Display version")
+	listenAddress = flag.String("l", "127.0.0.1:8080", "Web interface listen address")
+	version       = flag.Bool("v", false, "Display version")
 )
 
 func main() {
@@ -30,8 +29,7 @@ func main() {
 		os.Exit(0)
 	}
 
-	go webinterface()
-	go websockets()
+	go webserver()
 	go imagesWatcher()
 
 	// wait indefinitely
@@ -39,38 +37,28 @@ func main() {
 	<-done
 }
 
-func webinterface() {
+func webserver() {
+	// staticfiles
 	http.Handle("/", http.FileServer(assetFS()))
-	http.ListenAndServe(*listenAddress, nil)
-}
 
-func websockets() {
-	// A log scope allows you to customize the logging that websocketd performs.
-	//You can provide your own log scope with a log func.
+	// websocket
 	logScope := wsd.RootLogScope(wsd.LogAccess, func(l *wsd.LogScope,
 		level wsd.LogLevel, levelName string,
 		category string, msg string, args ...interface{}) {
 		fmt.Println(args...)
 	})
-
-	// Configuration options tell websocketd where to look for programs to
-	// run as WebSockets.
 	config := &wsd.Config{
 		ScriptDir:      "/opt/pravdabox/filters",
 		UsingScriptDir: true,
 		StartupTime:    time.Now(),
 		DevConsole:     false,
 	}
-
-	// Register your route and handler.
 	http.HandleFunc("/ws-bin/", func(rw http.ResponseWriter, req *http.Request) {
 		handler := http.StripPrefix("/ws-bin", wsd.NewWebsocketdServer(config, logScope, MAXFORKS))
 		handler.ServeHTTP(rw, req)
 	})
-	if err := http.ListenAndServe(*wsListenAddress, nil); err != nil {
-		fmt.Println("could not start server!", err)
-		os.Exit(1)
-	}
+
+	http.ListenAndServe(*listenAddress, nil)
 }
 
 func imagesWatcher() {
